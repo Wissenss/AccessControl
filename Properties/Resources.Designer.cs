7e22cac0e016ae77056b620859bd2b64f7f51135
mrbanana55 <dlegadoandres0502@gmail.com>
2023-06-10 21:36:10 -0600

    dibujo de piso 1

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -153,6 +153,16 @@ namespace AccessControl.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap piso_1 {
+            get {
+                object obj = ResourceManager.GetObject("piso 1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap purple_pencil {
             get {
                 object obj = ResourceManager.GetObject("purple pencil", resourceCulture);
